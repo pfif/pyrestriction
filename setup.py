6689da2eabd285b5d6222d165b9792770a1a3f09
DVNO (Florent) <pfifzehirman@yahoo.fr>
2015-02-28 14:09:34 +0000

    Version 1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = "PyRestriction",
     description = "Tell it the amounts that you owe or want to save, and it will compute how much money is avaliable to you.",
-    version = "1.0rc2",
+    version = "1.0",
     author = "Pfif Zehirman",
     url = "https://github.com/pfif/pyrestriction/",
     packages = ["pyrestriction"],
